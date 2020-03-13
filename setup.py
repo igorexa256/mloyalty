894410af03f841583e48b65c51c3912a80b9a875
Igor Belyansky <igorexa256@gmail.com>
2020-03-13 18:36:40 +0300

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mloyalty',
-    version='0.2.2',
+    version='0.2.3',
     packages=find_packages(),
     url='https://github.com/igorexa256/mloyalty',
     license='MIT',
